52342b29682f9994552926b6828df8906cd6f228
Zak Kolar <zakkolar@users.noreply.github.com>
2024-10-10 12:30:57 -0400

    style highlight play controls

--- src/components/HighlightDemo.vue
+++ src/components/HighlightDemo.vue
@@ -22,10 +22,10 @@ function startHighlight() {
     cursor.classList.add('ibeam');
     cursor.style.transition = '';
     cursor.style.left = `${first.offsetLeft}px`;
-    cursor.style.top = `${first.offsetTop + ((first.offsetHeight - cursor.offsetHeight)/2)}px`;
+    cursor.style.top = `${first.offsetTop + ((first.offsetHeight - cursor.offsetHeight) / 2)}px`;
 
     highlight();
-    cursor.style.transition = `left ${(speed.value * props.text.length)/1000}s linear`;
+    cursor.style.transition = `left ${(speed.value * props.text.length) / 1000}s linear`;
 
     cursor.style.left = `${last.offsetLeft + last.offsetWidth}px`
 }
@@ -34,8 +34,7 @@ function highlight() {
     highlightedPosition.value++;
     if (highlightedPosition.value < props.text.length) {
         timer.value = setTimeout(highlight, speed.value)
-    }
-    else {
+    } else {
         timer.value = setTimeout(startHighlight, 3000);
     }
 }
@@ -60,23 +59,40 @@ function play() {
     startHighlight();
 }
 
+function toggleAnimation() {
+    if (playAnimation.value) {
+        stop();
+    } else {
+        play();
+    }
+}
+
 </script>
 <template>
     <div class="container">
-        <span v-for="(character, index) of props.text" class="character" :class="{highlighted: highlightedPosition>index}">
+        <span v-for="(character, index) of props.text" class="character"
+              :class="{highlighted: highlightedPosition>index}">
       {{ character }}
   </span>
-        <button class="control" v-if="playAnimation" @click="stop">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm6-2.438c0-.724.588-1.312 1.313-1.312h4.874c.725 0 1.313.588 1.313 1.313v4.874c0 .725-.588 1.313-1.313 1.313H9.564a1.312 1.312 0 0 1-1.313-1.313V9.564Z" clip-rule="evenodd" />
-</svg>
+        <button class="control" @click="toggleAnimation">
+            <!--Stop button-->
+            <span v-if="playAnimation">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                     class="size-6">
+                <path fill-rule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm6-2.438c0-.724.588-1.312 1.313-1.312h4.874c.725 0 1.313.588 1.313 1.313v4.874c0 .725-.588 1.313-1.313 1.313H9.564a1.312 1.312 0 0 1-1.313-1.313V9.564Z"
+                      clip-rule="evenodd"/>
+            </svg>
+            </span>
 
-
-        </button>
-        <button class="control" v-else @click="play">
+            <!--Play button-->
+            <span v-else>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
-</svg>
+                <path fill-rule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+                      clip-rule="evenodd"/>
+            </svg>
+          </span>
 
         </button>
         <div id="cursor"></div>
@@ -94,7 +110,7 @@ function play() {
 
 #cursor.ibeam {
     background: #181818;
-     height: 0.8em;
+    height: 0.8em;
     width: 0.05em;
     --ibeam-width: 0.4em;
 }
@@ -105,11 +121,11 @@ function play() {
     height: 0.05em;
     background: inherit;
     position: absolute;
-    left:calc(calc(var(--ibeam-width) / -2) + 0.025em);
+    left: calc(calc(var(--ibeam-width) / -2) + 0.025em);
 }
 
 #cursor.ibeam:before {
-    top:0;
+    top: 0;
 }
 
 #cursor.ibeam:after {
@@ -124,7 +140,15 @@ button.control {
     background: transparent;
     border: none;
     font-size: 0.6em;
+    color: #c4c4c4;
+    transition: color 0.2s linear;
 }
+
+button.control:hover {
+    color: #222222;
+}
+
+
 button.control svg {
     width: 1em;
     height: 1em;
